d8d985e88c3ddae80d82bc8ebd33b4fea5ef7193
programista-nautil <programista@nautil.pl>
2025-07-04 09:22:01 +0200

    przeniesienie logiki albumow do osobnego pliku

--- src/app/album/[albumId]/page.tsx
+++ src/app/album/[albumId]/page.tsx
@@ -6,68 +6,11 @@ import Link from 'next/link'
 import { authOptions } from '@/app/api/auth/[...nextauth]/route'
 import Header from '@/app/components/Header'
 import { PhotoGrid } from './components/PhotoGrid'
-import { getDriveClient } from '@/lib/drive'
-
-interface Photo {
-	id: string
-	name: string
-	thumbnailLink?: string | null
-}
-
-interface Album {
-	name: string
-}
+import { getAlbumDetails, getPhotos, setInitialCover } from '@/lib/album-actions'
 
 type Props = {
 	params: { albumId: string }
 	searchParams: { [key: string]: string | string[] | undefined }
-}
-
-async function setInitialCover(folderId: string, fileId: string): Promise<{ newName: string }> {
-	const drive = getDriveClient()
-
-	const fileRes = await drive.files.get({ fileId: fileId, fields: 'name' })
-	const originalName = fileRes.data.name
-	if (!originalName) throw new Error('Could not get file name')
-
-	const nameWithoutCover = originalName.replace(/_cover/g, '')
-	const extensionIndex = nameWithoutCover.lastIndexOf('.')
-	let coverName: string
-
-	if (extensionIndex !== -1) {
-		const name = nameWithoutCover.substring(0, extensionIndex)
-		const extension = nameWithoutCover.substring(extensionIndex)
-		coverName = `${name}_cover${extension}`
-	} else {
-		coverName = `${nameWithoutCover}_cover`
-	}
-
-	await drive.files.update({
-		fileId: fileId,
-		requestBody: { name: coverName },
-	})
-
-	return { newName: coverName }
-}
-
-async function getAlbumDetails(albumId: string): Promise<Album> {
-	const drive = getDriveClient()
-	const response = await drive.files.get({
-		fileId: albumId,
-		fields: 'name',
-	})
-	return response.data as Album
-}
-
-async function getPhotos(albumId: string): Promise<Photo[]> {
-	const drive = getDriveClient()
-	const response = await drive.files.list({
-		q: `'${albumId}' in parents and mimeType contains 'image/' and trashed=false`,
-		fields: 'files(id, name, thumbnailLink)',
-		pageSize: 1000,
-	})
-
-	return (response.data.files as Photo[]) || []
 }
 
 export default async function AlbumPage({ params }: Props) {
