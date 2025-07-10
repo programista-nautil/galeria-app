0e0ce800cdd2eb63bd9a2ff58ca813f48acbef10
programista-nautil <programista@nautil.pl>
2025-07-10 08:48:00 +0200

    nowe maile

--- src/lib/permissions.ts
+++ src/lib/permissions.ts
@@ -1,4 +1,6 @@
 export const clientFolderMapping: { [email: string]: string } = {
 	'parafia.gmina.kosakowo@gmail.com': 'Parafia',
 	'kulturakosakowo@gmail.com': 'Muzeum',
+	'a11y.dostepnosc@gmail.com': 'A11y',
+	'aktywnekosakowo@gmail.com': 'Aktywne',
 }
